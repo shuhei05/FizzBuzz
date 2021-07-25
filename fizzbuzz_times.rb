3bb6e2da6b52c38127b25b89c7a6e1fff803cda7
shuhei05 <qoo0228y@gmail.com>
2021-07-25 23:20:06 +0900

    first_commit

---
+++ fizzbuzz_times.rb
@@ -0,0 +1,14 @@
+#0から30まで繰り返す
+1..31.times do |i|    
+
+  if i%15==0
+      puts "FizzBuzz!"
+  elsif i%3==0
+      puts "Fizz!"
+  elsif i%5==0 
+      puts "Buzz!"
+  else
+      puts i
+  end
+
+end
